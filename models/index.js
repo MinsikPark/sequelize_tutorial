71bf12e79e5190116ccac92fa0f2b80b73291409
minsikPark <pms3300@gmail.com>
2019-09-25 23:24:36 +0900

    모델 정의, DB연결 설정

---
+++ models/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const Sequelize = requir('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+
+const db = {}
+
+//DB연동
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+// model 매핑
+db.User = require('./user')(sequelize, Sequelize); 
+db.comment = require('./comment')(sequelize, Sequelize); 
+
+// 종속관계 설정
+db.User.hasMany(db.comment, {foreignKey : 'commenter', sourceKey : 'id'});
+db.comment.belongsTo(db.User, {foreignKey : 'commenter', targetKey :'id'});
+module.exports = db;
